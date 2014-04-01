1fb6009a064647b138413d966cece7c9506c5d32
Zachary Mink <zxm2164@rit.edu>
2014-04-01 00:18:27 -0400

    added folder

---
+++ Mechanics Project/AngryBirdsDriverLittleG.m
@@ -0,0 +1,269 @@
+function [struct,realTime] =  AngryBirdsDriverLittleG
+%created by Zach Mink and E. Backus April 2012
+%all lengths are supposed to be measured in red-bird diameters
+%note: coordinates are in approximate red bird lengths-units of measure
+%speed of red bird initially was recorded shortly after launch(probably a
+%bit small) 
+%data is probably taken in the software at every frame - given in units of
+%per second
+close all
+
+
+%%%%
+%DONT FORGET TO TAKE OUT THE POINTS AFTER THE PIG COLLISION EMILY!!!!!!!
+
+
+
+%:)
+%%%%
+
+BirdData = textread('BirdData2.txt');
+
+%constants
+    Asteroid1 = [24.86, -.656];     %position on screen
+    x1 = Asteroid1(1);
+    y1 = Asteroid1(2);
+    R1 = 5.688/2;                   %asteroid radius
+    fR1 = 28.99/2;                  %field radius
+    Asteroid2 = [52.7, -.656];      %position on screen
+    x2 = Asteroid2(1);
+    y2 = Asteroid2(2);
+    R2 = 5.688/2;                   %asteroid 2 radius
+    fR2 = 34.15/2;                  %field radius
+    
+    %[xpos,vx,ypos,vy] of the bird taken from tracker software
+    RedBird = [BirdData(1,2), BirdData(1,4),BirdData(1,3), BirdData(1,5)];
+    
+    xb0 = RedBird(1);
+    vxb0 = RedBird(2);
+    yb0 = RedBird(3); 
+    vyb0 = RedBird(4);    
+    mBird = 1;                      %mass of a red bird
+    g1 = 20;
+    g2 = 20;
+    %k = 2.5;
+    
+    for k = [2.5]
+    
+    constants = [x1, y1, x2, y2, g1, g2, k, mBird, fR1, fR2, R1, R2];
+
+    %characteristic length and time
+      xc = x1;
+      tau = sqrt(x1/g1);
+
+%solve the coupled ODEs
+    %specify the initial position and speed in unitless terms
+    init = [xb0./x1, vxb0./(x1/tau), yb0./x1, vyb0./(x1/tau)]; 
+    endTime = BirdData(end,1);
+    tSpan = [0 endTime/(tau)];%convert desired time into unitless time
+
+    options = odeset('Events', @eventsCollisionDetection);
+
+    struct = ode23s(@fAngryBirdDiffEQ, tSpan, init, options,constants);
+
+    %catch unitless output 
+        ut = struct.x;       %struct.x is unitless time T
+        ux = struct.y(1,:);   
+        uvx = struct.y(2,:);
+        uy = struct.y(3,:);
+        uvy = struct.y(4,:);
+
+    % %plot unitless variables
+    %         subplot(3,2,1)%positions versus time
+    %             plot(ut,ux,'r',ut,uy,'b')
+    %             legend('unitless x', 'unitless y')
+    %             xlabel('unitless time')
+    %         subplot(3,2,3) %velocities versus time
+    %             plot(ut,uvx,'r',ut,uvy,'b')
+    %             legend('unitless vx', 'unitless vy')
+    %             xlabel('unitless time')
+    %         subplot(3,2,5)%positions in space
+    %             plot(ux,uy,'r',x1/xc,y1/xc,'kx',x2/xc,y2/xc,'kx')
+    %             xlabel('unitless x')
+    %             ylabel('unitless y')
+    % 
+    % %plot real variables
+             realTime = ut*tau;
+      
+      %Shifting BirdData to t = 0
+      BirdData(:,1) = BirdData(:,1) - BirdData(1,1);
+             
+             
+    %         realX = ux*xc;
+    %         realY = uy*xc;
+    %         realVx = uvx*xc/tau;
+    %         realVy = uvy*xc/tau;
+    %         subplot(3,2,2)%positions versus time
+    %             plot(realTime,realX,'r',realTime,realY,'b')
+    %             legend('real x', 'real y')
+    %             xlabel('real time')
+    %         subplot(3,2,4) %velocities versus time
+    %             plot(realTime,realVx,'r',realTime,realVy,'b')
+    %             legend('real vx', 'real vy')
+    %             xlabel('real time')
+    %         subplot(3,2,6)%positions in space
+    %             plot(realX,realY,'r',x1,y1,'kx',x2,y2,'kx')
+    %             %axis([0 80 -40 40])
+    %             xlabel('real x')
+    %             ylabel('real y')
+    %             
+
+    %plot real variables
+    realX = ux*xc;
+    realY = uy*xc;
+    %positions in space
+        %define coordinates for gravitational circles and
+        %asteroid circles
+        N = 256;
+        theta = (0:N)*2*pi/N;
+        %Note: using the same radius for both gravity
+        %fields and asteroids
+        x = fR1*cos(theta) + x1;
+        xn = fR2*cos(theta) + x2;
+        y = fR1*sin(theta) + y1;
+        yn = fR2*sin(theta) + y2;
+        xA = R1*cos(theta) + x1;
+        yA = R1*sin(theta) + y1;
+
+    %plot(realX,realY,'r-',x1,y1,'kx',x2,y2,'kx')
+    figure
+    hold on
+    plot(realX,realY,'r--',x1,y1,'kx',x2,y2,'kx',x,y,'b-',xn, ...
+        yn,'b-',xA,yA,'k', xA+(x2-x1),yA + (y2-y1), 'k', BirdData(:,2),BirdData(:,3),'c')
+    plot(zeros(1,70), 'g')
+    %axis([0 80 -40 40])
+    xlabel('real x')
+    ylabel('real y')
+    title(strcat('k =',num2str(k),'g_{1}=',num2str(g1),'g_{2}=',...
+        num2str(g2)));
+    hold off
+    
+    
+    %Interpolating the data
+    
+    %equally spaced time values data will be interpolated across
+    interpTime = 0:.01:BirdData(end,1);
+    
+    %interpolated X values from the Tracker software - the 'real' values
+    interpXActual = interp1(BirdData(:,1),BirdData(:,2),interpTime,'spline');
+    %interpolated X values from the physical models
+    interpXNew = interp1(realTime,realX,interpTime,'spline');
+    %interpolated Y values from real data
+    interpYActual = interp1(BirdData(:,1),BirdData(:,3),interpTime,'spline');
+    %interpolated Y values from model
+    interpYNew = interp1(realTime,realY,interpTime,'spline');
+    
+    %calculate the residuals between the x and y values
+    xResiduals = interpXNew-interpXActual;
+    xResMean = mean(xResiduals);
+    yResiduals = interpYNew-interpYActual;
+    yResMean = mean(yResiduals);
+    
+    NewResSum = xResMean + yResMean;
+    
+    if NewResSum > OldResSum
+        %parameters;
+    end
+    
+    
+    
+    
+    figure
+    hold on
+        plot(interpTime,interpYActual,interpTime,interpYNew,interpTime,yResiduals);
+        plot(interpTime,interpXActual,'bl');
+        plot(BirdData(:,1),BirdData(:,2),'r');
+        plot(interpTime,interpXNew,'b');
+        plot(realTime,realX,'y');
+        plot(interpTime, xResiduals, 'g')
+    hold off
+    
+    
+    
+    
+      
+    end
+end
+
+function vPrime = fAngryBirdDiffEQ(~, v, constants)
+%constants
+    x1 = constants(1);
+    y1 = constants(2);
+    x2 = constants(3);
+    y2 = constants(4);
+    g1 = constants(5);
+    g2 = constants(6);
+    k  = constants(7);
+    mBird = constants(8);
+    fR1 = constants(9);
+    fR2 = constants(10);
+        
+   A = 1;       %boolean coefficient for first asteroid
+   B = 1;       %boolean coefficient for second asteroid
+   C = 0;       %boolean drag coefficient to turn on drag when inside either field
+    
+%initialize vPrime
+    vPrime = zeros(4,1);
+    
+%If the bird is outside of either asteroid field, remove the effects of
+%that asteroid field
+    if fR1^2 < (v(1)*x1 - x1)^2 + (v(3)*x1 - y1)^2   %outside asteriod1 field
+        A = 0;
+    end
+    
+    if fR2^2 < (v(1)*x1 - x2)^2 + (v(3)*x1 - y2)^2   %outside asteriod2 field
+        B = 0;
+    end
+
+%If the bird is inside either asteroid field, include drag
+   if (A == 1 || B == 1 )
+       C = 1;
+   end
+    
+    %differential equations of motion
+    vPrime(1) = v(2);
+    vPrime(2) = A*(1-v(1))./sqrt((1-v(1)).^2 + ((y1/x1)-v(3)).^2)...
+                + B*(g2/g1)*(x2/x1 - v(1))./sqrt(((x2/x1)-v(1)).^2 + ...
+                ((y2/x1)-v(3)).^2)...
+                - C*k/(g1*mBird) * v(2)./sqrt(v(2).^2 + v(4).^2);
+    vPrime(3) = v(4);
+    vPrime(4) = A*((y1/x1)-v(3))./sqrt((1-v(1)).^2 + ((y1/x1)-v(3)).^2)...
+                + B*(g2/g1)*(y2/x1 - v(3))./sqrt(((x2/x1)-v(1)).^2 + ...
+                ((y2/x1)-v(3)).^2)...
+                - C*k/(g1*mBird) * v(4)./sqrt(v(2).^2 + v(4).^2);
+            
+end
+
+
+
+%determines if bird has collided with an asteroid, if so, terminates
+%the ode solver    
+function [value,isTerminal,dir] = eventsCollisionDetection(~,v,constants)
+
+    %acquire all of the constants needed for this function
+    x1 = constants(1);
+    y1 = constants(2);
+    x2 = constants(3);
+    y2 = constants(4);
+    R1 = constants(11);
+    R2 = constants(12);
+    
+    %define the variables if neither if statement is true
+    value = 1;
+    isTerminal = 0;
+    dir = 0;
+    
+     if R1^2 > (v(1)*x1 - x1)^2 + (v(3)*x1 - y1)^2%bird is inside asteriod1
+        value = 0;
+        isTerminal = 1;
+        dir = 0;
+     end
+    
+     if R2^2 > (v(1)*x1 - x2)^2 + (v(3)*x1 - y2)^2%bird is inside asteriod2
+        value = 0;
+        isTerminal = 1;
+        dir = 0;
+     end
+end
+
+ 
